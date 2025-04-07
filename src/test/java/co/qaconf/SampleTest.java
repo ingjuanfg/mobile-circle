ddc61bc12489e626d6a507f854e7935d1958d748
ingjuanfg <juan.isfg@gmail.com>
2025-04-07 00:17:05 -0500

    Configurando yml file

--- src/test/java/co/qaconf/SampleTest.java
+++ src/test/java/co/qaconf/SampleTest.java
@@ -28,13 +28,12 @@ public class SampleTest {
                 .setAppActivity("com.google.android.youtube.HomeActivity")
                 .setIsHeadless(true)
                 .setAutoGrantPermissions(true);
-        // Evita que se intente borrar las políticas de hidden API y se realice la inicialización del dispositivo
+        // Agregamos estas capabilities para omitir la inicialización que causa el error
         options.setCapability("skipHiddenApiCheck", true);
         options.setCapability("skipDeviceInitialization", true);
-
+        options.setCapability("skipServerInstallation", true);
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
     }
-
 
 
     @Test
